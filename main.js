46738b5f694fef4cd00a96649d64431fdae50466
Anju0307 <jpmantilla01@misena.edu.co>
2022-11-08 11:33:45 -0500

    Desarrollo ejercicio 2

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    
+    addEventListener("submit", (e) => {
+        e.preventDefault();
+        let entrada = Object.fromEntries(new FormData(e.target));
+        console.log(entrada);
+        let h = entrada.horas
+        let pags = 5208.33
+        pago = h*pags
+
+        console.log(`${pago}`)
+        document.body.insertAdjacentHTML("beforeend", `<div class="tecto">el pago de nomina que le corresponde es: ${pago}<div>`)
+    })
 })
