d35435aa3f128e4fa975e3d9a29da97f8a6186f9
Zulay Ortegon <zulyortegon15@gmail.com>
2022-11-14 12:36:50 -0500

    Archivo 4

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-const n = document.getElementById("n");
-const d = document.getElementById("d");
+const nvendedores = document.getElementById("nvendedores");
+const sueldob = document.getElementById("sueldo");
 
 const respuesta = document.getElementById("resp");
 const calcular = document.getElementById("calcular");
 calcular.addEventListener('click' , proceso);
 
 function proceso(){
-    const N = parseFloat(n.value);
-    const D = parseFloat(d.value);
-     acumulador = 0
-     rta = "¡El número SI es perfecto 🥳!"
-
-    if(N >= 1 && D >=1){
-        if( N % D == 0){
-            for(let i = 1; i <= N / 2; ++i){
-                if(N % i == 0){
-                    acumulador += i
-                }
-            }
-            if(acumulador != N){
-                rta = "El número NO es perfecto 😕"
-            }
-        }
+    const n = parseFloat(nvendedores.value);
+    const sueldo = parseFloat(sueldob.value); 
+    x=1
+    while (x <= n){  
+        x += 1  
+        let v1 = Number(prompt("Ingrese el valor de venta 1"));
+        let v2 = Number(prompt("Ingrese el valor de venta 2"));
+        let v3 = Number(prompt("Ingrese el valor de venta 3"));
     
-        respuesta.innerText= rta
-    }else{
-        respuesta.innerText="NECIOO 😠,Vuelve a intentarlo con los parámetros establecidos 😒"
-    }  
+        comision = (v1 + v2 + v3) * 0.10
+        
+        respuesta.innerText= "El sueldo mensual es: " + sueldo + "\n La comisión del mes es: " + comision + "\n El sueldo mensual es: " + (sueldo+comision)
+    }
+
 }
