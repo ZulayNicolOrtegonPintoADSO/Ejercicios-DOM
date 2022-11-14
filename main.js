a2352594dcc858786c87e70fd08f0b70334718a1
Zulay Ortegon <zulyortegon15@gmail.com>
2022-11-14 03:41:47 -0500

    ejercicio 3

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-addEventListener("DOMContentLoaded", (e)=>{
-    addEventListener("submit", (e) => {
-        e.preventDefault();
-        let entrada = Object.fromEntries(new FormData(e.target));
-        console.log(entrada);
-        let h = entrada.horas
-        let pags = 5208.33
-        pago = h*pags
+const n = document.getElementById("n");
+const d = document.getElementById("d");
 
-        console.log(`${pago}`)
-        document.body.insertAdjacentHTML("beforeend", `<div class="tecto">el pago de nomina que le corresponde es: ${pago}<div>`)
-    })
-})
+const respuesta = document.getElementById("resp");
+const calcular = document.getElementById("calcular");
+calcular.addEventListener('click' , proceso);
+
+function proceso(){
+    const N = parseFloat(n.value);
+    const D = parseFloat(d.value);
+     acumulador = 0
+     rta = "¡El número SI es perfecto 🥳!"
+
+    if(N >= 1 && D >=1){
+        if( N % D == 0){
+            for(let i = 1; i <= N / 2; ++i){
+                if(N % i == 0){
+                    acumulador += i
+                }
+            }
+            if(acumulador != N){
+                rta = "El número NO es perfecto 😕"
+            }
+        }
+    
+        respuesta.innerText= rta
+    }else{
+        respuesta.innerText="NECIOO 😠,Vuelve a intentarlo con los parámetros establecidos 😒"
+    }
+
+    
+}
